9af6d26a15221e733de38b7eb6c38b4440e82642
diego <driverag22@gmail.com>
2025-03-04 11:47:57 +0100

    Renamed to candidateBuilder

---
+++ cpp/candidateBuilder.cpp
@@ -0,0 +1,19 @@
+#include "candidateBuilder.h"
+
+/// Edge flipping operation to modify the graph in the search process.
+/// This is intended to reduce triangles in the complement and potentially increase the chromatic number.
+void edgeFlip(Graph& g, Edge e) {
+
+}
+
+/// Simulated annealing based method to search for a graph of thickness two with chromatic number at least [targetChromatic].
+/// This function sets-up the initial solution and iteratively refines it.
+void simulatedAnnealing(Graph& g, int targetChromatic) {
+
+}
+
+/// Swarm intelligence based method to search for a graph of thickness two with chromatic number at least [targetChromatic].
+/// This function sets-up a population of candidate solutions and guides them toward higher chromatic numbers.
+void swarmIntelligence(Graph& g, int targetChromatic) {
+
+}
---
+++ cpp/candidateBuilder.h
@@ -0,0 +1,27 @@
+#ifndef GRAPHBUILDER_H
+#define GRAPHBUILDER_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <boost/graph/adjacency_list.hpp>
+
+#include "helperFunctions.h"
+
+using namespace boost;
+using namespace std;
+
+/// Edge flipping operation to modify the graph in the search process.
+/// This is intended to reduce triangles in the complement and potentially increase the chromatic number.
+void edgeFlip(Graph& g, Edge e);
+
+/// Simulated annealing based method to search for a graph of thickness two with chromatic number at least [targetChromatic].
+/// This function sets-up the initial solution and iteratively refines it.
+void simulatedAnnealing(Graph& g, int targetChromatic);
+
+/// Swarm intelligence based method to search for a graph of thickness two with chromatic number at least [targetChromatic].
+/// This function sets-up a population of candidate solutions and guides them toward higher chromatic numbers.
+void swarmIntelligence(Graph& g, int targetChromatic);
+
+#endif // GRAPHBUILDER_H
+
