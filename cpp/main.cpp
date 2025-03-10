2536537017f01810925097f4e38702b7301bd86b
diego <driverag22@gmail.com>
2025-03-10 13:36:01 +0100

    Found candidate graph to disprove conjecture

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,24 +2,19 @@
 #include "candidateBuilder.h"
 
 int main() {
-    printProgressBar(0, 6, 
-                     "Iteration n = 9");
-    for (int n1 = 9; n1 <= 15; n1++) {
-        int n = 2*n1;
-        maxEdges = 3 * n - 6;
-        // wheel graph with two middle vertices
-        auto* edgesFirst = wheelGraphEdge(n1, 2);
-        /* printEdges(*edgesFirst); */
-        auto* edges = blowup(edgesFirst, n1);
-        /* cout << endl; */
-        /* printEdges(*edges); */
-        testBiplanarity(edges, n);
-        printProgressBar(n1-9, 6, "Iteration n = " + to_string(n1));
-        delete edgesFirst;
-        delete edges;
-    }
+    /* int n1 = 9; */
+    /* int n = 2*n1; */
+    /* maxEdges = 3 * n - 6; */
+    /* // wheel graph with two middle vertices */
+    /* auto* edgesFirst = wheelGraphEdge(n1, 2); */
+    /* auto* edges = blowup(edgesFirst, n1); */
+    /* delete edgesFirst; */
+    /* preprocessEdges(edges, n); */
+    /* printEdges(*edges); */
+    /* testBiplanarity(edges, n); */
+    /* delete edges; */
     // (numVertLow, numVertHigh, numAttempts, bool independenceNumbe, bool chromaticNumber)
-    /* computeCandidateGraphs(20, 20, 1000000, true, true); */
+    computeCandidateGraphs(20, 80, 100000, true, true);
     return 0;
 }
 
