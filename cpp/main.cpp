ff84a6b73d77b55073f7e4bbfaa55cbeb6e91b93
Diego RiveraGarrido <drivera@eu-login-34.euler.ethz.ch>
2025-03-31 11:57:58 +0200

    candidate finder branch

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,12 +3,12 @@
 #include "biplanarSAT.h"
 
 int main() {
-    int n = 3;
-    int num_vertices = n + 2;
-    vector<Edge> edges = k4stack(n);
+    //int n = 3;
+    //int num_vertices = n + 2;
+    //vector<Edge> edges = k4stack(n);
     /* edges = blowup(edges, num_vertices); */
-    Graph g = constructGraph(edges, num_vertices);
-    outputGraph(g);
+    //Graph g = constructGraph(edges, num_vertices);
+    //outputGraph(g);
     /* auto start = chrono::high_resolution_clock::now(); */
     /* if (isBiplanarSAT(edges, num_vertices)) { */
     /*     cout << "biplanarrr" << endl; */
@@ -18,7 +18,7 @@ int main() {
     /* cout << "Took: " << elapsed.count() << " seconds.\n"; */
 
     // (numVertLow, numVertHigh, numAttempts, bool independenceNumbe, bool chromaticNumber)
-    /* computeCandidateGraphs(20, 80, 100000, true, true); */
+    computeCandidateGraphs(60, 80, 1500000, true, true);
     return 0;
 }
 
